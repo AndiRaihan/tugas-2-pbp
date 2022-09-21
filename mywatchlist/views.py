24b2149b86dc629f93c7a3a78e7f682248c0b7c4
AndiRaihan <dzaky.raihan@gmail.com>
2022-09-21 22:53:02 +0700

    Tried to do bonus

--- mywatchlist/views.py
+++ mywatchlist/views.py
@@ -6,10 +6,21 @@ from django.core import serializers
 # Create your views here.
 def show_html(request):
     data = WatchlistMovies.objects.all()
+    sum_watched_movies = 0
+    half_of_movies = len(data)/2
+    for movies in data.iterator():
+        if movies.watched:
+            sum_watched_movies +=1
+        
+    if sum_watched_movies >= half_of_movies:
+        msg = "Selamat, kamu sudah banyak menonton!"
+    else:
+        msg = "Wah, kamu masih sedikit menonton!"
     context = {
         'list_film' : data,
         'nama' : 'Andi Muhamad Dzaky Raihan',
         'npm' : '2106631412',
+        'Message' : msg,
     }
     return render(request, 'watchlist.html', context)
     
